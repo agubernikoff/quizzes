f1d3dc0af6d6347b7e2ca4057bc49f639b54b0c6
alexander gubernikoff <agubernikoff@gmail.com>
2022-03-09 21:06:58 -0500

    leaderboard working

--- src/LeaderBoard.js
+++ src/LeaderBoard.js
@@ -1,12 +1,38 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 function LeaderBoard() {
-
-
-
-
-
-  return <div>LeaderBoard</div>;
+  const [leaders, setLeaders] = useState([]);
+  useEffect(
+    () =>
+      fetch("http://localhost:3001/leaders")
+        .then((resp) => resp.json())
+        .then((data) => setLeaders(data)),
+    []
+  );
+  console.log(leaders);
+  const leadersFR = leaders.sort((b, a) => a.score - b.score).slice(0, 10);
+  const mappedLeaders = leadersFR.map((leader) => (
+    <tr>
+      <td>{leader.name}</td>
+      <td>{leader.score}</td>
+      <td>{leader.category}</td>
+    </tr>
+  ));
+  return (
+    <div>
+      <h1>LeaderBoard</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Score</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>{mappedLeaders}</tbody>
+      </table>
+    </div>
+  );
 }
 
 export default LeaderBoard;
